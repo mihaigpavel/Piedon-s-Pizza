49a2b3673f06488f893559d2b9f9fe7ad48eaa6e
mihaigpavel <mihai.gpavel@gmail.com>
2025-03-30 19:49:41 +0300

    definire clase v0.13

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include <Helper.h>
 #include <string>
+
 class Document {
 private:
     std::string dataEliberare;
@@ -14,7 +15,7 @@ private:
 public:
     // constructor
     Document(const std::string &data_eliberare, const std::string &data_expirare, const std::string &emitent,
-        const std::string &numar, const std::string &serie)
+             const std::string &numar, const std::string &serie)
         : dataEliberare(data_eliberare),
           dataExpirare(data_expirare),
           emitent(emitent),
@@ -24,7 +25,7 @@ public:
 
 
     // Destructor
-    ~Document(){
+    ~Document() {
         std::cout << "Document deleted" << std::endl;
     }
 
@@ -36,6 +37,7 @@ public:
           numar(other.numar),
           serie(other.serie) {
     }
+
     Document(Document &&other) noexcept
         : dataEliberare(std::move(other.dataEliberare)),
           dataExpirare(std::move(other.dataExpirare)),
@@ -44,7 +46,7 @@ public:
           serie(std::move(other.serie)) {
     }
 
-    Document & operator=(const Document &other) {
+    Document &operator=(const Document &other) {
         if (this == &other)
             return *this;
         dataEliberare = other.dataEliberare;
@@ -55,7 +57,7 @@ public:
         return *this;
     }
 
-    Document & operator=(Document &&other) noexcept {
+    Document &operator=(Document &&other) noexcept {
         if (this == &other)
             return *this;
         dataEliberare = std::move(other.dataEliberare);
@@ -66,31 +68,42 @@ public:
         return *this;
     }
 
-    [[nodiscard]] const std::string & get_data_eliberare() const {
+    [[nodiscard]] const std::string &get_data_eliberare() const {
         return dataEliberare;
     }
 
-    [[nodiscard]] const std::string & get_data_expirare() const {
+    [[nodiscard]] const std::string &get_data_expirare() const {
         return dataExpirare;
     }
 
-    [[nodiscard]] const std::string & get_emitent() const {
+    [[nodiscard]] const std::string &get_emitent() const {
         return emitent;
     }
 
-    [[nodiscard]] const std::string & get_numar() const {
+    [[nodiscard]] const std::string &get_numar() const {
         return numar;
     }
 
-    [[nodiscard]] const std::string & get_serie() const {
+    [[nodiscard]] const std::string &get_serie() const {
         return serie;
     }
 
-
+    friend std::ostream &operator<<(std::ostream &os, const Document &obj) {
+        os << "//////////////////////////////////////////////" << '\n';
+        os << "Data eliberare: " << obj.get_data_eliberare() << '\n';
+        os << "Data expirare: " << obj.get_data_expirare() << '\n';
+        os << "Emitent: " << obj.get_emitent() << '\n';
+        os << "Numar: " << obj.get_numar() << '\n';
+        os << "Serie: " << obj.get_serie() << '\n';
+        os << "//////////////////////////////////////////////" << '\n';
+        return os;
+    }
 };
 
 
 int main() {
 
+    Document doc1("01-01-2023", "01-01-2024", "Emitent1", "123456", "AB");
+    std::cout<<doc1;
     return 0;
 }
