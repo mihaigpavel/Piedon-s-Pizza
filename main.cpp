cc0e7decbb62e4a3e9ae9d1f80c96b61b7369801
mihaigpavel <mihai.gpavel@gmail.com>
2025-04-16 01:56:34 +0300

    modificare afisiere buletin

--- main.cpp
+++ main.cpp
@@ -228,22 +228,24 @@ public:
         return datePersonale;
     }
     friend std::ostream &operator <<(std::ostream &os, const CarteIdentitate &p) {
-        os << "Carte de identitare\n";
-        os << "////////////////////////////////////////////////////////////////////\n";
-        os << "ROUMANIE   " << "ROMANIA   " << "ROMANIA\n";
-        os << "SERIA " << p.document.get_serie() << "  NR " << p.document.get_numar() << '\n';
+        os << "CARTE DE IDENTITATE\n";
+        os << "/////////////////////////////////////////////////////////////////\n";
+        os << "ROUMANIE                     ROMANIA                     ROMANIA\n";
+        os << "SERIA: " << p.document.get_serie() << "     NR: " << p.document.get_numar() << '\n';
+        os << "CNP: " << p.datePersonale.get_cnp() << '\n';
         os << "Nume/Nom/Last name\n";
         os << p.datePersonale.get_nume() << '\n';
         os << "Prenume/Prenom/First name\n";
         os << p.datePersonale.get_prenume() << '\n';
-        os << "Cetatenie/Nationalite,NAtionality\n" << p.cetatenia << '\n';
-        os << "Loc nastere/Lieu de naissance/Place of birth\nJud." << p.adresaNastere.get_judet() << " Loc." << p
-                .adresaNastere.get_localitate() << '\n';
-        os << "Domiciliu/Adresse/address\n" << p.adresaDomiciliu;
-        os << "Emis de/Delivree par/issued by       valabilitate/validite/validity\n";
-        os << p.document.get_emitent() << "              " << p.document.get_data_eliberare() << "-" << p.
-                document.get_data_expirare() << '\n';
-        os << "////////////////////////////////////////////////////////////////////\n";
+        os << "Cetatenie/Nationalite/Nationality\n" << p.cetatenia << '\n';
+        os << "Loc nastere/Lieu de naissance/Place of birth\n";
+        os << "Jud. " << p.adresaNastere.get_judet_prescurtat() << " Loc. " << p.adresaNastere.get_localitate() << '\n';
+        os << "Domiciliu/Adresse/Address\n" << p.adresaDomiciliu;
+        os << "Emis de/Delivre par/Issued by      Valabilitate/Validite/Validity\n";
+        os << p.document.get_emitent() << "                      "
+           << p.document.get_data_eliberare() << " - "
+           << p.document.get_data_expirare() << '\n';
+        os << "/////////////////////////////////////////////////////////////////\n";
         return os;
     }
 };
